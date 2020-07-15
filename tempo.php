41bd87e9d8979fe78ae0ac7872d0ed16785a7fce
Marcus Marçal <marcus.marcal@gmail.com>
2020-07-15 20:23:41 -0300

    Adicionado novo e-mail

--- tempo.php
+++ tempo.php
@@ -7,7 +7,7 @@ Sempre arredonda para o inteiro mais alto.
 Sintaxe: int tempo(str $data1, str $hora1, srt $data2, str $hora2);
 Exemplo: tempo("1981-03-20","06:00:00",date("Y-m-d"),date("H:i:s");
 Retorna o valor em horas, desde a data do meu nascimento até hoje (!!)
-By Bluverts - bluverts@gmail.com
+By Bluverts - bluverts@gmail.com (= Marcus Marçal - marcus.marcal@gmail.com)
 
 */
 
