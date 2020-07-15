1f675f2cfb4c39c1a5b1f780337e78742bbfab44
Marcus Marçal <marcus.marcal@gmail.com>
2020-07-15 20:03:41 -0300

    Atualização de teste

--- tempo.php
+++ tempo.php
@@ -1,3 +1,4 @@
+<?php
 /*
 Função de cálculo de horas.
 Pega duas datas e duas horas e calcula o quanto tempo em horas tem de uma a outra.
@@ -13,11 +14,10 @@ By Bluverts - bluverts@gmail.com
 $tempo = 0;
 
 function tempo($data1,$hora1,$data2,$hora2){
-
-$i = split(":",$hora1);
-$j = split("-",$data1);
-$k = split(":",$hora2);
-$l = split("-",$data2);
+  $i = split(":",$hora1);
+  $j = split("-",$data1);
+  $k = split(":",$hora2);
+  $l = split("-",$data2);
 
 $tempo1 = mktime($i[0],$i[1],$i[2],$j[1],$j[2],$j[0]);
 $tempo2 = mktime($k[0],$k[1],$k[2],$l[1],$l[2],$l[0]);
@@ -29,3 +29,4 @@ $GLOBALS['tempo'] = ceil((($tempo2 - $tempo1)/60)/60);
 }
 
 tempo ("2006-09-11", "21:00:00", date ("Y-m-d"), date ("H").":00:00");
+ ?>
